d9900a1f43a7c2c2323e7bb9e2e13d6c5305cad0
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-08-06 15:12:56 +0700

    Dark mode (#4)

---
+++ Dark_Mode/darkmode.js
@@ -0,0 +1,17 @@
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+});
